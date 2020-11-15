4d235cd185aa2e47323ce4061da42b4d17e613bc
Teo-Reat <teo.reat@gmail.com>
2020-11-15 23:37:56 +0300

    Completed alpha

--- src/app/main.js
+++ src/app/main.js
@@ -1,2 +1,6 @@
-const button = document.getElementById('nav-icon1')
-button.onclick = (() => button.classList.toggle('open'))
+const button = document.getElementById('nav-icon1');
+const menu = document.getElementById('nav-menu');
+button.onclick = (() => {
+	button.classList.toggle('open');
+	menu.classList.toggle('top-menu__inner--close')
+})
